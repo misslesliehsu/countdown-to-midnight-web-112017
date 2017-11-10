6cf0b1811d4d1762351f6fa4b2c5e9fff2c06628
Leslie Hsu <chuckleberries@hotmail.com>
2017-11-10 22:19:11 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(times)
-  while times > 0 
+  while times > 0
     puts "#{times} SECOND(S)!"
     times -= 1
   end
-  "HAPPY NEW YEAR!"  
+  "HAPPY NEW YEAR!"
 end
