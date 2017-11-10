be3a865353b4de59253d298c4a1cfd277fb6175b
Leslie Hsu <chuckleberries@hotmail.com>
2017-11-10 22:21:28 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(times)
+  while times > 0
+    puts "#{times} SECOND(S)!"
+    times -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(times)
+  while times > 0
+    puts "#{times} SECOND(S)!"
+    times -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
